dc93548fa89bd3463359f3a92a27734018d56dca
Jonathan Villegas <jvillegas86@gmail.com>
2026-01-26 20:00:17 -0300

    ending call to action section

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,42 +1,31 @@
 export default function modal(){
-/*
-     if (document.readyState === 'loading') {
-        document.addEventListener('DOMContentLoaded', createModal);
-    } else {
-        createModal(); 
-    }
-*/
-    function createModal() {
-        
 
-        
+           
        //creating modal elements
         const modalSection = document.getElementById("modal");
+
         const myModal = document.createElement("dialog");
-        const modalHeader = document.createElement("div");
         const closeBtn = document.createElement("button");
         const modalBody = document.createElement("div"); 
         modalBody.classList.add("modal-body");
         const modalContent = document.createElement("p");
         const subscribeBtn = document.createElement("button");
         
-        modalBody.appendChild(modalContent);
-        modalBody.appendChild(subscribeBtn);
-        //adding content to the elements
+      
+
         closeBtn.classList.add("close-modal");
         myModal.id = "call-to-action";
         closeBtn.innerHTML= "x";
         modalContent.innerHTML = "Sign up to the site and get the member benefits! Push the button below and continue with the registration process"
         subscribeBtn.innerHTML = "Sign Up"
 
-        //appending elements to the html id
-
-        myModal.appendChild(modalContent);
-        myModal.appendChild(subscribeBtn);
-        myModal.appendChild(modalHeader);
+        myModal.appendChild(closeBtn);
+        modalBody.appendChild(modalContent);
+        modalBody.appendChild(subscribeBtn);
         myModal.appendChild(modalBody);
-        modalHeader.appendChild(closeBtn);
-        modalHeader.classList.add("modal-header");
+    
+
+        
 
         modalSection.appendChild(myModal)
 
@@ -48,12 +37,10 @@ export default function modal(){
         } );
 
         
+
         
     }
    
- createModal();
-
-
-}
 
 modal();
+
